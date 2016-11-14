a0846568874ed31e7073134fc7f0c15e496597ea
WetPatootie <jerardmichaelaquino5@gmail.com>
2016-11-14 18:03:09 +0800

    Update Luner.js
    Version 1.2

--- Luner.js
+++ Luner.js
@@ -1,4 +1,7 @@
-<script>
+var Names = ["MEMES", "HARDER DADDY :tayrexx:", "LE", "Fearless Swiftie is Awesome", "Holeguy", "WHERES MY FUCKING PACKAGE!!!!"];
+var Daddy = Names[Math.floor(Math.random() * Names.length)];
+var bg = []
+
 function meme(dank, meme)
 {
   if (i < 50)
@@ -16,10 +19,16 @@ function luna(lun, ner, lol)
   }
 }
 
-//Can't stop the luner :crescent_moon: :nerd:
-for (var i = 0; i < 1000; i++) {
+function nene() {
+  document.getElementById("meh").innerHTML = Daddy;
+}
+
+function myFunction() {
+    document.body.style.backgroundColor = "#f3f3f3";
+    document.body.style.backgroundImage = "url('http://i.imgur.com/zPP2tWb.png')";
+}
+
+for (var i = 100; i < 100; i++) {
   meme("You Tried ", "Escaping The Luner for " + i  + " Times.");
   luna("Stop Trying ", "you can't escape the luner, you've been trying for " + i + " Times");
 }
-
-</script>
