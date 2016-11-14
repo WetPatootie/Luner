ac76076a0cc88a7e74737783082428d9a2ded9bc
WetPatootie <jerardmichaelaquino5@gmail.com>
2016-11-14 14:48:14 +0800

    Add files via upload
    The JS Script

---
+++ Luner.js
@@ -0,0 +1,24 @@
+<script>
+function meme(dank, meme)
+{
+  if (i < 50)
+  {
+    alert(dank + meme);
+  }
+}
+meme("Can't Escape ", "The Luner");
+
+function luna(lun, ner, lol)
+{
+  if (i >= 50)
+  {
+    alert(lun + ner);
+  }
+}
+
+for (var i = 0; i < 1000; i++) {
+  meme("You Tried ", "Escaping The Luner for " + i  + " Times.");
+  luna("Stop Trying ", "you can't escape the luner, you've been trying for " + i + " Times");
+}
+
+</script>
