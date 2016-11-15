9be1928b3c403586434d70c1ba5c8c21a924de0b
WetPatootie <jerardmichaelaquino5@gmail.com>
2016-11-16 05:58:35 +0800

    Update Luner.js

--- Luner.js
+++ Luner.js
@@ -1,23 +1,27 @@
-var Names = ["MEMES", "HARDER DADDY :tayrexx:", "LE", "Fearless Swiftie is Awesome", "Holeguy", "WHERES MY FUCKING PACKAGE!!!!"];
+var Names = ["MEMES", "HARDER DADDY :tayrexx:", "LE", "Fearless Swiftie is Awesome", "Holeguy", "WHERES MY FUCKING PACKAGE!!!!", "CosGOD"];
 var Daddy = Names[Math.floor(Math.random() * Names.length)];
-var bg = []
 
-function meme(dank, meme)
-{
-  if (i < 50)
-  {
-    alert(dank + meme);
-  }
+function FS() {
+    for (var i = 0; i <= 100; i++) {
+  meme("You Tried ", "Escaping The Luner for " + i  + " Times.");
+  luna("Stop Trying ", "you can't escape the luner, you've been trying for " + i + " Times");
 }
-meme("Can't Escape ", "The Luner");
+}
 
-function luna(lun, ner, lol)
-{
-  if (i >= 50)
-  {
+
+var i = 0;
+function meme(dank, dork, Donran) {
+    if (i < 50) {
+        alert(dank + dork);
+    }
+}
+
+function luna(lun, ner, lol) {
+  if (i >= 50) {
     alert(lun + ner);
   }
 }
+
 
 function nene() {
   document.getElementById("meh").innerHTML = Daddy;
@@ -25,10 +29,13 @@ function nene() {
 
 function myFunction() {
     document.body.style.backgroundColor = "#f3f3f3";
-    document.body.style.backgroundImage = "url('http://i.imgur.com/zPP2tWb.png')";
+    document.body.style.backgroundImage = "url('http://i.imgur.com/YTNRPWE.jpg')";
 }
 
-for (var i = 100; i < 100; i++) {
-  meme("You Tried ", "Escaping The Luner for " + i  + " Times.");
-  luna("Stop Trying ", "you can't escape the luner, you've been trying for " + i + " Times");
+function Name() {
+    var Andri
+    
+    Andri=prompt("What's Your Name Buddy?");
+    document.getElementById("rexx").innerHTML = "Welcome to Tay Is Bae Website, " + Andri +  "!";
 }
+Name();
